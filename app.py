5d561d29781e5a863e765fc3225e213c49bd7e8c
sarthakarcader-create <sarthak.arcader@gmail.com>
2026-09-11 23:03:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -177,7 +177,7 @@ div[data-testid="stMarkdownContainer"] > div {
 .hero-message {
     position: absolute;
     z-index: 3;
-    left: 74.6vw;
+    left: 71.5vw;
     top: 42.5vh;
     margin: 0 !important;
     padding: 0 !important;
@@ -200,7 +200,7 @@ div[data-testid="stMarkdownContainer"] > div {
    state transition remains reliable. */
 div[data-testid="stButton"] {
     position: fixed !important;
-    left: 74.6vw !important;
+    left: 71.5vw !important;
     top: 77.6vh !important;
     width: 196px !important;
     height: 52px !important;
@@ -285,13 +285,13 @@ div[data-testid="stButton"] > button:focus-visible {
         font-size: clamp(3rem, 7.4vw, 5.4rem) !important;
     }
     .hero-message {
-        left: 69vw;
+        left: 66vw;
         top: 44vh;
         font-size: clamp(2.35rem, 5vw, 4.1rem) !important;
         line-height: 1.06 !important;
     }
     div[data-testid="stButton"] {
-        left: 69vw !important;
+        left: 66vw !important;
         top: 77vh !important;
         width: 176px !important;
         height: 48px !important;
